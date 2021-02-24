f664eead45d49cce2c75132b750deada00457055
lireys451 <79550945+lireys451@users.noreply.github.com>
2021-02-23 17:15:13 -0800

    Create Hello_World.cpp

---
+++ Lab/Hello_World.cpp
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout<<"Hello World!";
+    system("PAUSE");
+    return 0;
+}
